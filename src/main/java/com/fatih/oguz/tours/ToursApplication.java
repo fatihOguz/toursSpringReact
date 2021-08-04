ee1716ed7a2266baa0c567fb93c688fce3525a9c
FatihOguz <foguz4999@gmail.com>
2021-08-04 15:21:45 +0300

    Initial commit

---
+++ src/main/java/com/fatih/oguz/tours/ToursApplication.java
@@ -0,0 +1,13 @@
+package com.fatih.oguz.tours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatih/oguz/tours/ToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatih.oguz.tours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
